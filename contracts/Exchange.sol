29482ffbbb77621a2c145e74e1db8b3fa6118b42
Mom Jon <momjon@Moms-iMac.attlocal.net>
2022-09-20 11:42:34 -0400

    10. Withdraw tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ import "./Token.sol";
     mapping(address => mapping(address => uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw (address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -25,6 +26,18 @@ import "./Token.sol";
 
         // Emit an event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+
+        require(tokens[_token][msg.sender] >= _amount);
+
+        Token(_token).transfer(msg.sender, _amount);
+
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+
     }
 
     function balanceOf(address _token, address _user)
