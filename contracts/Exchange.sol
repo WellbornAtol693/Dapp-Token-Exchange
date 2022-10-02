40e11893e7999ff81d3d3a079f6633fa6ef83418
Mom Jon <momjon@Moms-iMac.attlocal.net>
2022-10-02 01:47:22 -0400

    14. Seed exchange

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -75,6 +75,7 @@ import "./Token.sol";
     function depositToken(address _token, uint256 _amount) public {
         // Transfer tokens to exchange
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        
 
         // Update user balance
         tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
